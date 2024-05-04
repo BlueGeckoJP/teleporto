534b741fc15167e59d36096798b574217af6dd5a
bluegecko <kei09doya@gmail.com>
2024-05-04 14:43:29 +0900

    added webserver func

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,19 @@
-use std::{
-    env,
-    fs::File,
-    io::{BufWriter, Write},
-};
+#![deny(elided_lifetimes_in_paths)]
 
-use axum::{
-    body::Bytes,
-    extract::{DefaultBodyLimit, Multipart},
-    routing::post,
-    Router,
-};
-use tokio::net::TcpListener;
-use tower_http::trace::TraceLayer;
+mod sender;
+mod webserver;
 
 #[macro_use]
 extern crate log;
-extern crate env_logger;
+
+use std::thread;
+
+use webserver::webserver;
 
 #[tokio::main]
 async fn main() {
-    env::set_var("RUST_LOG", "trace");
-    env_logger::init();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("trace"));
 
-    let ip = "127.0.0.1:8080";
-
-    let app = Router::new()
-        .route("/", post(accept_form))
-        .layer(DefaultBodyLimit::disable())
-        .layer(TraceLayer::new_for_http());
-
-    let listener = TcpListener::bind(ip).await.unwrap();
-
-    info!("Web server started at {}", ip);
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn accept_form(mut multipart: Multipart) {
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        let file_name = field.file_name().unwrap().to_string();
-        let content_type = field.content_type().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
-        trace!(
-            "Length of `{name}` (`{file_name}`: `{content_type}` is {} bytes",
-            data.len()
-        );
-        write_to_file(data, file_name).await;
-    }
-}
-
-async fn write_to_file(content: Bytes, file_name: String) {
-    let home_dir = home::home_dir().unwrap();
-    let path = home_dir.join("Downloads").join(file_name);
-
-    let f = File::create(path).unwrap();
-    let mut writer = BufWriter::new(f);
-    writer.write_all(&content.to_vec()).unwrap();
-    writer.flush().unwrap();
+    let ws = thread::spawn(move || webserver());
+    ws.join().unwrap().await;
 }
---
+++ src/webserver.rs
@@ -0,0 +1,56 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+};
+
+use axum::{
+    body::Bytes,
+    extract::{DefaultBodyLimit, Multipart},
+    routing::post,
+    Router,
+};
+use log::info;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+
+pub async fn webserver() {
+    let ip = "127.0.0.1:8080";
+
+    let app = Router::new()
+        .route("/", post(accept_form))
+        .layer(DefaultBodyLimit::disable())
+        .layer(TraceLayer::new_for_http());
+
+    let listener = TcpListener::bind(ip).await.unwrap();
+
+    info!("Web server started at {}", ip);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn accept_form(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let file_name = field.file_name().unwrap().to_string();
+        let content_type = field.content_type().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        trace!(
+            "`{file_name}`: Content-Type: `{content_type}`, {} bytes",
+            data.len()
+        );
+
+        write_to_file(data, file_name).await;
+    }
+}
+
+async fn write_to_file(content: Bytes, file_name: String) {
+    let home_dir = home::home_dir().unwrap();
+    let path = home_dir.join("Downloads").join(file_name);
+
+    let f = File::create(&path).unwrap();
+    let mut writer = BufWriter::new(f);
+    writer.write_all(&content.to_vec()).unwrap();
+    writer.flush().unwrap();
+
+    info!("Saved at `{}`", path.to_str().unwrap());
+}
